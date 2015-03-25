60058a3f47e247d5ca86afb7170234f09c463106
jaseew01 <jseewer@twc.com>
2015-03-25 15:27:07 -0400

    Added implementation to scoring class Issue #22

--- app/scoring.js
+++ app/scoring.js
@@ -16,39 +16,60 @@ proto = {
 	//@param score: the score that you want to add
 	//@return: the updated list of scores
 	addScore: function(score) {
-		//
+		this.scores.push(score);
+		this.scores.sort(function(a,b) {
+			return a-b;
+		});
+		this.length += 1;
+		return this.scores;
 	},
 
 	//@desc: Will remove the score at a certain index in the scores array
 	//@param index: the index of the score you want removed
 	//@return: the updated list of scores
 	removeScoreAtIndex: function(index) {
-		//
+		this.scores.splice(index,1);
+		this.length -= 1;
+		return this.scores;
 	},
 
 	//@desc: removes the last item in the scores array
 	//@return: the updated list of scores
 	removeLowest: function() {
-		//
+		this.scores.pop();
+		this.scores.length -= 1;
+		return this.scores;
+	},
+
+	//@desc: removes the first item in the scores array
+	//@return: the updated list of scores
+	removeHighest: function() {
+		this.scores.splice(0,1);
+		this.scores.length -= 1;
+		return this.scores;
 	},
 
 	//@desc: will get the highest score in the scores array
 	//@return: the highest score in the scores array
 	getHighScore: function() {
-		//
+		return this.scores[0];
 	},
 
 	//@desc: decides if score is the current high score
 	//@param score: the score to check
 	//@return: true or false based on whether or not score is the high score
 	isHighScore: function(score) {
-		//
+		if(score === this.getHighScore()) {
+			return true;
+		} else {
+			return false;
+		}
 	},
 
 	//@desc: gets the lowest score in the scores array
 	//@return: the lowest score in the scores array
 	getLowestScore: function() {
-		//
+		return this.scores[this.length - 1];
 	}
 };
 
