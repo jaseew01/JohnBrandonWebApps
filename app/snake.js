25d58347421358e52e1eb564893da35322153d2a
kelleyb15 <bmk4993@gmail.com>
2015-03-26 17:18:15 -0400

    snake.js passing most eslint tests
    2 errors I'm unsure of.

--- app/snake.js
+++ app/snake.js
@@ -1,7 +1,7 @@
 var proto, Obj;
 
-//@desc: this class will handle the snake moving around the page
-//@param bodySize: the width/height of each square in the snake body
+// @desc: this class will handle the snake moving around the page
+// @param bodySize: the width/height of each square in the snake body
 function newSnake(bodySize) {
 	var lst;
 
@@ -12,46 +12,50 @@ function newSnake(bodySize) {
 	lst.directions = ["North", "South", "East", "West"];
 	lst.bodySize = bodySize;
 	lst.moveCoordinates = {
-		North: {x:0, y:bodySize},
-		South: {x:0, y:-bodySize},
-		East: {x:bodySize, y:0},
-		West: {x:-bodySize, y:0}
+		North: {x: 0, y: bodySize},
+		South: {x: 0, y: -bodySize},
+		East: {x: bodySize, y: 0},
+		West: {x: -bodySize, y: 0}
 	};
 
 	Array.prototype.unique = function() {
-	    var o = {}, i, l = this.length, r = [];
-	    for(i=0; i<l;i+=1) o[""+this[i].x+""+this[i].y] = this[i];
-	    for(i in o) r.push(o[i]);
-	    return r;
+		var o = {}, i, l = this.length, r = [];
+		for (i = 0; i < l; i += 1) {
+			o["" + this[i].x + "" + this[i].y] = this[i];
+		}
+		for (i in o) {
+			r.push(o[i]);
+		}
+		return r;
 	};
 
 	return lst;
 }
 
 proto = {
-	//@desc: Will change the direction of the snake
-	//@param dir: Snakes new direction
+	// @desc: Will change the direction of the snake
+	// @param dir: Snakes new direction
 	changeDirection: function(dir) {
-		if(this.directions.contains(dir)) {
+		if (this.directions.contains(dir)) {
 			this.direction = dir;
 		} else {
 			throw new Error("Invalid Direction");
 		}
 	},
 
-	//@desc: Will add one box onto the body of the snake
+	// @desc: Will add one box onto the body of the snake
 	addOne: function() {
-		var currTail = this.getTail();
-		var newTail = {x:currTail.x, y:currTail.y+this.bodySize};
+		var currTail = this.getTail(), newTail;
+		newTail = {x: currTail.x, y: currTail.y + this.bodySize};
 		this.snakeBody.push(newTail);
 
 		this.length += 1;
 	},
 
-	//@desc: Will move the snake in the current direction
-	//	Each item in this.snakeBody will take on the coordinates
-	//	of the item before itself in the array.
-	//	The first item in the array will move in the direction of this.direction
+	// @desc: Will move the snake in the current direction
+	// 	Each item in this.snakeBody will take on the coordinates
+	// 	of the item before itself in the array.
+	// 	The first item in the array will move in the direction of this.direction
 	move: function() {
 		var newObj = {}, distChange, head;
 		distChange = this.moveCoordinates[this.direction];
@@ -64,37 +68,37 @@ proto = {
 		this.snakeBody.unshift(newObj);
 	},
 
-	//@desc: Will get the tail of the snake body
-	//@return: the last object in the snake body
+	// @desc: Will get the tail of the snake body
+	// @return: the last object in the snake body
 	getTail: function() {
-		return this.snakeBody[this.length-1];
+		return this.snakeBody[this.length - 1];
 	},
 
-	//@desc: Will get the head of the snake body
-	//@return: the first object in the snake body
+	// @desc: Will get the head of the snake body
+	// @return: the first object in the snake body
 	getHead: function() {
 		return this.snakeBody[0];
 	},
 
-	//@desc: Checks to see if the head of the snake hits any part of the snake body
-	//@return: boolean
+	// @desc: Checks to see if the head of the snake hits any part of the snake body
+	// @return: boolean
 	checkCollision: function() {
-		if(this.snakeBody.unique().length < this.length) {
+		if (this.snakeBody.unique().length < this.length) {
 			return true;
 		} else {
 			return false;
 		}
 	},
 
-	//@desc: Will handle detecting if the snake head is currently
-	//	out of the bounds of the page
-	//@param width: the width of the game area
-	//@param height: the height of the game area
-	//@return: Boolean value dependant on it being out of bounds or not
+	// @desc: Will handle detecting if the snake head is currently
+	// 	out of the bounds of the page
+	// @param width: the width of the game area
+	// @param height: the height of the game area
+	// @return: Boolean value dependant on it being out of bounds or not
 	checkOutOfBounds: function(width, height) {
-		head = this.getHead();
-		if(0 < head.x < width) {
-			if(0 < head.y < height) {
+		var head = this.getHead();
+		if (head.x > 0 && head.x < width) {
+			if (head.y > 0 && head.y < height) {
 				return false;
 			}
 		} else {
@@ -104,7 +108,7 @@ proto = {
 };
 
 Obj = {
-	new:newSnake
+	new: newSnake
 };
 
 Object.defineProperty(Obj, "prototype", {
