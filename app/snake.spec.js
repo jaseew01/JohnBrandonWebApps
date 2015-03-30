b11204c600708a689271cae3175c6ec92018d978
kelleyb15 <bmk4993@gmail.com>
2015-03-29 21:17:17 -0400

    snake constructor passing tests

--- app/snake.spec.js
+++ app/snake.spec.js
@@ -10,3 +10,15 @@
 
  Snake = require('./snake.js');
 
+var mySnake = Snake.new();
+
+describe("Testing the snake constructor", function() {
+	"use strict";
+	it("Testing that it has a length of 1", function() {
+		expect(mySnake.length).to.equal(1);
+	});
+
+	it("Testing the default direction is North", function () {
+		expect(mySnake.direction).to.equal("North");
+	});
+});
